6042ce3046714374863fc56ae07993b93404410a
Noam Okman <noamokman@gmail.com>
2017-11-11 19:57:28 +0200

    feat(init): init now asks for overwrite

--- src/cli/actions/init.js
+++ src/cli/actions/init.js
@@ -1,11 +1,39 @@
+import {BOOL} from 'caporal';
+import inquirer from 'inquirer';
 import {registerCommand} from '../util';
-import {init} from '../..';
+import {init, info} from '../..';
 
 registerCommand({
   name: 'init',
   description: 'Set the path to the cloud folder',
   args: [['<path>', 'Cloud folder path']],
-  action ({args: {path}, logger}) {
-    logger.info(init(path));
+  opts: [['-f, --force', 'force overwrite', BOOL, false]],
+  action ({args: {path}, options: {force}, logger}) {
+    const initialize = () => {
+      init(path);
+      logger.info('Cloud link initialized.');
+    };
+
+    const {cloudPath} = info();
+
+    if (!cloudPath || force) {
+      return initialize();
+    }
+
+    logger.info(`Cloud link already initialized in \`${cloudPath}\`.`);
+
+    return inquirer.prompt([{
+      type: 'confirm',
+      name: 'overwrite',
+      message: 'Do you want to overwrite?',
+      default: false
+    }])
+      .then(({overwrite}) => {
+        if (!overwrite) {
+          return;
+        }
+
+        initialize();
+      });
   }
 });
--- src/cli/util.js
+++ src/cli/util.js
@@ -8,11 +8,15 @@ export const notifier = updateNotifier({pkg});
 
 export const notify = () => notifier.notify();
 
-export const registerCommand = ({name, description, initialization, action, args = []}) => {
+export const registerCommand = ({name, description, initialization, action, args = [], opts = []}) => {
   const command = program.command(name, description);
 
   args.forEach(argument => {
     command.argument(...argument);
+  });
+
+  opts.forEach(option => {
+    command.option(...option);
   });
 
   command
