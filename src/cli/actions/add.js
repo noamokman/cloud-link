523625f9723a309cdf5558bf4eb549eff5e10751
Noam Okman <noamokman@gmail.com>
2017-12-12 17:05:15 +0200

    feat(cli): fixed add action

--- src/cli/actions/add.js
+++ src/cli/actions/add.js
@@ -2,10 +2,24 @@ import {BOOL} from 'caporal';
 import {registerCommand} from '../util';
 import {add, list} from '../..';
 
-const getSrcByName = name => {
-  const link = list().find(link => link.name === name);
+const getSrcByName = name => list()
+  .then(links => {
+    const link = links.find(link => link.name === name);
 
-  return link && link.src;
+    return link && link.src;
+  });
+
+const resolveSrc = (src, name) => {
+  const srcPromise = src ? Promise.resolve(src) : getSrcByName(name);
+
+  return srcPromise
+    .then(src => {
+      if (!src) {
+        return Promise.reject(new TypeError('Could not find the source from the given name, that means this is a new link and need to be provided with the source as well.'));
+      }
+
+      return src;
+    });
 };
 
 registerCommand({
@@ -15,13 +29,13 @@ registerCommand({
   args: [['<name>', 'name of the link'], ['[src]', 'relative path to source inside the cloud folder'], ['<dest>', 'path to the destination']],
   opts: [['-a, --apply', 'apply the link', BOOL, true]],
   action ({options: {apply}, args: {name, src, dest}, logger}) {
-    src = src || getSrcByName(name);
-
-    if (!src) {
-      return Promise.reject(new TypeError('Could not find the source from the given name, that means this is a new link and need to be provided with the source as well.'));
+    if (!dest) {
+      dest = src;
+      src = null;
     }
 
-    return add({name, src, dest, apply})
+    return resolveSrc(src, name)
+      .then(src => add({name, src, dest, apply}))
       .then(report => {
         logger.info(`Link ${name} added successfully!`);
 
